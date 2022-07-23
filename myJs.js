104504899059615074160cca5956fa85af5d9998
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2022-07-23 22:02:51 +0700

    Update myJs.js

--- myJs.js
+++ myJs.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/cuteCat.jpg",
+      imageUrl: "cuteCat.jpg",
       imageWidth: 300,
       imageHeight: 300,
       background: '#fff url("iput-bg.jpg")',
